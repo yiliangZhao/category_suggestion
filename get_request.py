ff2e265a4b2a88c4d6b052612d29670fb7545639
Zhao Yiliang <zhaoyiliang@gmail.com>
2017-10-25 11:24:14 +0800

    change IP address for testing

--- get_request.py
+++ get_request.py
@@ -1,9 +1,10 @@
 #coding: utf-8
+import sys
 import requests
 import time
 
 if __name__ == '__main__':
-    url = "http://203.116.23.228:5000/category/v1.1/item?itemname=日本%20takara%20tomy%20可%20愛達%20草莓%20水果%20屋%20臺中%20現貨%20可自取"
+    url = "http://localhost:5000/category/v1.1/item?itemname=日本%20takara%20tomy%20可%20愛達%20草莓%20水果%20屋%20臺中%20現貨%20可自取"
     for i in range(1000):
     	start = time.time()
     	response = requests.get(url)
