b8ea180b0719d6ecbd583cbca7ba52abadaa7da6
Zhao Yiliang <zhaoyiliang@gmail.com>
2017-10-25 22:55:27 +0800

    change get request test

--- get_request.py
+++ get_request.py
@@ -5,10 +5,10 @@ import time
 
 if __name__ == '__main__':
     url = "http://localhost:5000/category/v1.1/item?itemname=日本%20takara%20tomy%20可%20愛達%20草莓%20水果%20屋%20臺中%20現貨%20可自取"
-    for i in range(1000):
+    for i in range(10):
     	start = time.time()
     	response = requests.get(url)
     	result = response.json()
-    	# print(response.status_code, result)
+    	print(response.status_code, result)
     	print (time.time() -start)
 
