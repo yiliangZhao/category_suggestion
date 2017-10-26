b6eab5c8351fbb29c6218146effbbfa8cdb5c732
Zhao Yiliang <zhaoyiliang@gmail.com>
2017-10-26 10:51:21 +0800

    update post request port

--- post_request.py
+++ post_request.py
@@ -4,7 +4,7 @@ import time
 import sys
 
 if __name__ == '__main__':
-    url = 'http://203.116.23.228:5000/category/v1.1/list_items'
+    url = 'http://203.116.23.228:32143/category/v1.1/list_items'
     headers = {'content-type': 'application/json'}
     data = {"items": [[500571, 67000], [556324, 67000], [982869, 67000], [1919083, 67000], [1919522, 67000],
                       [1920312, 67000], [1920598, 67000], [1920740, 67000], [1921333, 67000], [1922152, 67000],
