66e2f44a1fd5426f6d003e1f77fa25afee220fd1
Zhao <zhaoyiliang@gmail.com>
2017-11-03 16:34:41 +0800

    add test for local

---
+++ get_request_local.py
@@ -0,0 +1,14 @@
+#coding: utf-8
+import sys
+import requests
+import time
+
+if __name__ == '__main__':
+    url = "http://localhost:32143/category/v1.1/item?title=日本%20takara%20tomy%20可%20愛達%20草莓%20水果%20屋%20臺中%20現貨%20可自取"
+    for i in range(10):
+    	start = time.time()
+    	response = requests.get(url)
+    	result = response.json()
+    	print(response.status_code, result)
+    	print (time.time() -start)
+
